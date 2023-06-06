f014e1cfe0151baf14cd7062aa1e4e41cbfa8e52
SkriLL <timurkazah1998@gmail.com>
2023-06-06 16:25:44 +0500

    header adapt

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -6,13 +6,19 @@
                     <router-link to="/" class="logo">
                         <img src="@/assets/images/Logo.svg" alt="">
                     </router-link>
-                    <ul class="nav__list">
-                        <li v-for="link in links" :key="link.title">
+                    <button class="nav__btn" @click="burgerOn">
+                        <img src="@/assets/images/menu.svg" alt="">
+                    </button>
+                    <ul class="nav__list" :class="{ active: burger }">
+                        <li class="nav__close" @click="burgerOff">
+                            <img src="@/assets/images/closeMenu.svg" alt="">
+                        </li>
+                        <li v-for="link in links" :key="link.title" @click="burger = false">
                             <router-link :to="link.url" class="nav__link">
                                 {{ link.title }}
                             </router-link>
                         </li>
-                        <li>
+                        <li @click="burger = false">
                             <router-link to="/search" class="nav__link">
                                 <img src="@/assets/images/search.svg" alt="">
                             </router-link>
@@ -31,6 +37,16 @@ const links = ref([
     { title: 'Фильмы', url: '/movie' },
     { title: 'Сериалы', url: '/tv' },
 ])
+
+const burger = ref(false)
+const burgerOn = () => {
+    burger.value = true
+    document.body.style.overflow = 'hidden'
+}
+const burgerOff = () => {
+    burger.value = false
+    document.body.style.overflow = 'visible'
+}
 
 
 </script>
