f13cc1a74ae56047c0a4d19e2e336a1cd64fa1ea
SkriLL <timurkazah1998@gmail.com>
2023-06-23 16:16:18 +0500

    popular request

--- src/main.js
+++ src/main.js
@@ -5,7 +5,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import '@/assets/style/main.scss'
+
+import VueLazyLoad from 'vue-lazyload'
+import errorImage from '@/assets/images/error.png'
+
+
 const app = createApp(App)
+
+app.use(VueLazyLoad, {
+    error: errorImage
+})
 
 app.use(createPinia())
 app.use(router)
